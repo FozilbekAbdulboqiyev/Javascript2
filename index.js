c548618a40755bec2f5bfbf0653f92b1b985212a
Fozilbek <fozilbekabdulboqiyev52@gmail.com>
2025-05-14 14:34:34 +0500

    17-Homework

---
+++ index.js
@@ -0,0 +1,21 @@
+// 1
+// let fruits = ["Apples", "Pear", "Orange"];
+
+// let shoppingcart = fruits;
+// shoppingcart.push("Banana");
+
+// console.log(fruits.length); 
+
+//2?
+
+
+// 3
+// let arr = ["a", "b"];
+
+// arr.push(function(){
+//     alert( this );
+// });
+// arr[2]();
+// function 
+
+//4
